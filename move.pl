6972b1c2522ce002b08d095a835d86a0412a6cd2
sadhukhan <sadhukhan@users.noreply.github.com>
2015-06-22 17:18:21 +0530

    move.pl
    Creates and copies a list of files to a desired directory.

---
+++ move.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+  use strict;
+  use warnings;
+
+my $dirname='########';
+
+my $names = '###########';
+
+my @name = split('\n', $names);     # Delimiter
+
+`mkdir $dirname`;
+foreach my $i(@name){
+`cp $i $dirname`;}
+
+print "Done\n";
